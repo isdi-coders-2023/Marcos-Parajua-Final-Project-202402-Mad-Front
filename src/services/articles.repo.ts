ba23345906e9a03e4c362c4456f5abcf4eb6f3c7
marcos parajua <marcosparajua@gmail.com>
2024-05-19 21:27:27 +0200

    change environment to production

--- src/services/articles.repo.ts
+++ src/services/articles.repo.ts
@@ -1,34 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Article } from '../entities/article.js';
-import { environment } from '../environments/environment.dev';
+import { environment } from '../environments/environment.js';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+ providedIn: 'root',
 })
 export class RepoArticlesService {
-  httpClient = inject(HttpClient);
-  url = environment.apiUrl + '/articles';
+ httpClient = inject(HttpClient);
+ url = environment.apiUrl + '/articles';
 
-  getById(id: string): Observable<Article> {
-    return this.httpClient.get<Article>(`${this.url}/${id}`);
-  }
+ getById(id: string): Observable<Article> {
+  return this.httpClient.get<Article>(`${this.url}/${id}`);
+ }
 
-  create(data: FormData) {
-    const url = this.url;
-    return this.httpClient.post(url, data);
-  }
+ create(data: FormData) {
+  const url = this.url;
+  return this.httpClient.post(url, data);
+ }
 
-  getArticles() {
-    return this.httpClient.get<Article[]>(this.url);
-  }
+ getArticles() {
+  return this.httpClient.get<Article[]>(this.url);
+ }
 
-  deleteArticle(id: string) {
-    return this.httpClient.delete<Article>(this.url + '/' + id);
-  }
+ deleteArticle(id: string) {
+  return this.httpClient.delete<Article>(this.url + '/' + id);
+ }
 
-  update(id: string, article: FormData): Observable<Article> {
-    return this.httpClient.patch<Article>(`${this.url}/${id}`, article);
-  }
+ update(id: string, article: FormData): Observable<Article> {
+  return this.httpClient.patch<Article>(`${this.url}/${id}`, article);
+ }
 }
